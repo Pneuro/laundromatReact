1002c7098bcd7c858c0f01556c41a4000296e3d3
Pneuro <57775304+Pneuro@users.noreply.github.com>
2020-09-27 20:47:47 -0400

    adding nav

--- src/App.js
+++ src/App.js
@@ -7,22 +7,30 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Promise from "./components/Promise";
 import Footer from "./components/Footer";
-
-import CalculatorForm from './WashFoldCalculator/CalculatorForm'
-import Shop from './components/Shop/Shop'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import CalculatorForm from "./WashFoldCalculator/CalculatorForm";
+import Shop from "./components/Shop/Shop";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Promise />
-      <About />
-      <Coin />
-      <WashFold />
-      <Commercial />
-      <CalculatorForm />
-      
+      <Router>
+        <Route path="/" exact>
+          <Promise />
+          <About />
+          <Coin />
+          <WashFold />
+          <Commercial />
+        </Route>
+        <Route path="/calculator">
+          <CalculatorForm />
+        </Route>
+        <Route path="/shop">
+          <Shop />
+        </Route>
+      </Router>
       <Contact />
       <Footer />
     </div>
